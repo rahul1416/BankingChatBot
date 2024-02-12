295c5bd9eac230b121c3d83c1579798131d57bf4
anant <ananty@iitbhilai.ac.in>
2024-02-12 23:13:28 +0530

    Created another Branch

--- bankingchatbotproject/bankingchatbotproject/urls.py
+++ bankingchatbotproject/bankingchatbotproject/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-import chatbotapp.urls
+import chatbotapp.urls 
 
 urlpatterns = [
     path('',include(chatbotapp.urls)),
